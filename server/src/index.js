b085fdc0dfc5af407fea6557d5c8b20362582ae6
Mika Munterud <mika.munterud@outlook.com>
2023-04-09 08:54:16 +0200

    renaming index.js to server.js and adding cors

--- server/src/index.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const { db } = require("./database");
-const server = express();
-server.use(express.json());
-
-server.listen(5050);
---
+++ server/src/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const server = express();
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+server.use(express.json());
+server.use(cookieParser());
+
+server.use(
+  cors({
+    origin: "http://localstorage:3000",
+    credentials: true,
+  })
+);
+
+server.listen(5050);
