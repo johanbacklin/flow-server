f8097fb015b5a7c8a2466fa0de731b6e5522fae5
Mika Munterud <mika.munterud@outlook.com>
2023-04-11 16:30:20 +0200

    Adding route for authentication & updating origin from localstorage to localhost

--- src/server/server.js
+++ src/server/server.js
@@ -2,13 +2,18 @@ const express = require("express");
 const server = express();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const { authenticationRoute } = require("../routes/authenticationRoute");
 
 server.use(express.json());
 server.use(cookieParser());
 
 server.use(
   cors({
-    origin: "http://localstorage:3000",
+    origin: "http://localhost:3000",
     credentials: true,
   })
 );
+
+server.use("/authentication", authenticationRoute);
+
+exports.server = server;
