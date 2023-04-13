2206581b6d5af7ddf4037d4449da8770909765bd
Andreas.Warncke <andreas.warncke.1996@gmail.com>
2023-04-13 12:14:59 +0200

    added /users to authenticationRoute
    also added checkAuthentication to /users

--- src/routes/authenticationRoute.js
+++ src/routes/authenticationRoute.js
@@ -5,6 +5,13 @@ const {
 
 const { userLogin } = require("../controllers/authenticationRoute/userLogin");
 
+const {
+  getAllUsers,
+} = require("../controllers/authenticationRoute/getAllUsers");
+const {
+  checkAuthentication,
+} = require("../controllers/middleware/checkAuthentication");
+
 const authenticationRoute = express.Router();
 
 /*
@@ -16,5 +23,7 @@ authenticationRoute.post("/login", userLogin);
  * This route makes it possible for a user to register another user to this application.
  */
 authenticationRoute.post("/register", userRegister);
+
+authenticationRoute.get("/users", checkAuthentication, getAllUsers);
 
 exports.authenticationRoute = authenticationRoute;
