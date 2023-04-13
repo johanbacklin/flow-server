6fdc5fb98661693ab1818e9ed959435dfca7a0ed
TereseBo <terese.boderus@gmail.com>
2023-04-13 09:24:31 +0200

    Add posts middleware, endpoint, and controller

--- src/routes/postsRoute.js
+++ src/routes/postsRoute.js
@@ -1,10 +1,18 @@
 const express = require("express");
+//Middlewares
+const{checkAuthentication }=require("../controllers/middleware/checkAuthentication")
+//Controllers
+const {addPost}=require("../controllers/postsRoute/addPost")
 
 const postsRoute = express.Router();
+
+postsRoute.use(checkAuthentication )
 
 postsRoute.get("/",(req,res)=>{
     res.status(200).send('Not implemented yet')
 })
 
+
+postsRoute.post('/add', addPost)
 
 exports.postsRoute= postsRoute;
