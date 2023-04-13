10e08e119639f9d97c0252a5658e037370e4bd7f
TereseBo <terese.boderus@gmail.com>
2023-04-13 11:53:16 +0200

    Started function for adding post

--- src/controllers/postsRoute/addPost.js
+++ src/controllers/postsRoute/addPost.js
@@ -1,4 +1,36 @@
+const { db } = require("../../database/database");
+const { validatePost } = require("../validations/validatePost")
 
 exports.addPost = function (req, res) {
-    res.status(200).send('reached addPost')
+
+    const validatedBody = validatePost(req.body)
+    if (validatedBody.error) {
+        response.status(400).send(validatedBody.error.details[0].message);
+        return;
+    }
+    const { postText } = validatedBody.value;
+
+    const newPost = {
+        username: req.loggedInUser,
+        postText,
+        likes: [],
+        comments: [],
+        $setOnInsert: { creation: new Date() }
+        //creation:new Date(<integer>)
+    }
+    db.posts.insertOne(newPost)
+        .then(result => {
+            console.log(result)
+        //    if(result.insertedID)
+            res.status(200).send('reached addPost')
+        })
+        .catch(error=>{
+            response.status(500).send('Ooops, something went wrong');
+            return
+        })
+
+
+    
 }
+
+//posts (postText, username, creation - date?), likes[], comment [(commentTex, username, creation - date)]
