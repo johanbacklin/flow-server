63a836e8af6467f547ddb1b8c34170ff97a14384
TereseBo <terese.boderus@gmail.com>
2023-04-13 11:53:36 +0200

    Renamed schema for cookie validation

--- src/controllers/validations/validateCookie.js
+++ src/controllers/validations/validateCookie.js
@@ -1,12 +1,12 @@
 const joi = require("joi");
 
 exports.validateCookie = function (loggedInUser) {
-  const postSchema = joi.object({
+  const cookieSchema = joi.object({
     id: joi.string().length(24).required(),
     username: joi.string().min(3).max(50).required(),
     iat: joi.number().required(),
     exp:joi.number().required()
   });
 
-  return postSchema.validate(loggedInUser);
+  return cookieSchema.validate(loggedInUser);
 };
