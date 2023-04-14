2bc360c1f23f5688546d16d2bc93da176ec66fb2
TereseBo <terese.boderus@gmail.com>
2023-04-14 11:26:53 +0200

    Added optional id in post validation

--- src/controllers/validations/validatePost.js
+++ src/controllers/validations/validatePost.js
@@ -3,6 +3,7 @@ const joi = require("joi");
 exports.validatePost = function (requestBody) {
   const postSchema = joi.object({
     postText: joi.string().min(3).max(500).required(),
+    id: joi.string().length(24).optional()
   });
 
   return postSchema.validate(requestBody);
