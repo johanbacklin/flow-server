2d673ee612c88670aa1b23e43a923b3146b5593a
Mika Munterud <mika.munterud@outlook.com>
2023-04-14 22:34:07 +0200

    Adding tests for userRegister endpoint

--- tests/controllers/authenticationRoute/userRegister.test.js
+++ tests/controllers/authenticationRoute/userRegister.test.js
@@ -1,23 +1,27 @@
 const request = require("supertest");
 const { server } = require("../../../src/server/server");
+const { db } = require("../../../src/database/database");
 
-describe("Testing userLogin endpoint", function () {
-  test("POST /authentication/login should return 400 if bad request body", async function () {
+beforeAll(async function () {
+  await db.connect();
+});
+
+afterAll(async function () {
+  await db.disconnect();
+});
+
+describe("Testing userRegister endpoint", function () {
+  test("POST /authentication/register should return 400 if bad request body", async function () {
     const response = await request(server)
-      .post("/authentication/login")
+      .post("/authentication/register")
       .send({});
     expect(response.status).toBe(400);
   });
 
-  /*
-   * Nedan test funkar inte än, måste kolla upp kopplingen till databasen varför den delen inte funkar
-   */
-  /*
-  test("POST /authentication/login should return 200 if good login", async function () {
+  test("POST /authentication/register should return 209 if username is taken", async function () {
     const response = await request(server)
-      .post("/authentication/login")
-      .send({ username: "Mika", password: "Choklad" });
-    expect(response.status).toBe(200);
+      .post("/authentication/register")
+      .send({ username: "test", password: "testingAccount" });
+    expect(response.status).toBe(409);
   });
-*/
 });
