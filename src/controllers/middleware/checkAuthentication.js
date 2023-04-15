b58da668639369d7e1220416c4fc66adb2cae997
Mika Munterud <mika.munterud@outlook.com>
2023-04-16 01:33:18 +0200

    Removing validation for cookie since this is a middleware and not an endpoint

--- src/controllers/middleware/checkAuthentication.js
+++ src/controllers/middleware/checkAuthentication.js
@@ -1,19 +1,14 @@
 const jwt = require("jsonwebtoken");
 const secret = process.env.SECRET;
 
-const{validateCookie}=require("../validations/validateCookie")
-
 /*
  * This middleware checks if a user is logged in by checking if a cookie exists containing the user information.
  */
 exports.checkAuthentication = function (request, response, next) {
   const authenticationToken = request.cookies.authenticationToken;
   try {
-    const loggedInUser = validateCookie(jwt.verify(authenticationToken, secret)).value;
-    if (!loggedInUser) {
-      throw new Error('Bad cookie, get another one')
-    }
-    
+    const loggedInUser = jwt.verify(authenticationToken, secret);
+
     request.loggedInUser = loggedInUser;
     next();
   } catch (error) {
