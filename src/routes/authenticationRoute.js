956ef37edba66c61f7eee3dbf78f4b73c20662a7
Mika Munterud <mika.munterud@outlook.com>
2023-04-15 20:31:09 +0200

    Adding endpoints deleteUser and logout to Authentication route

--- src/routes/authenticationRoute.js
+++ src/routes/authenticationRoute.js
@@ -4,6 +4,11 @@ const {
 } = require("../controllers/authenticationRoute/userRegister");
 
 const { userLogin } = require("../controllers/authenticationRoute/userLogin");
+const {
+  checkAuthentication,
+} = require("../controllers/middleware/checkAuthentication");
+const { userLogout } = require("../controllers/authenticationRoute/userLogout");
+const { userDelete } = require("../controllers/authenticationRoute/userDelete");
 
 const authenticationRoute = express.Router();
 
@@ -16,5 +21,12 @@ authenticationRoute.post("/login", userLogin);
  * This route makes it possible for a user to register another user to this application.
  */
 authenticationRoute.post("/register", userRegister);
+
+authenticationRoute.post("/logout", checkAuthentication, userLogout);
+
+/*
+ * This route makes it possible for a user to delete themselves from this application.
+ */
+authenticationRoute.delete("/deleteUser", checkAuthentication, userDelete);
 
 exports.authenticationRoute = authenticationRoute;
