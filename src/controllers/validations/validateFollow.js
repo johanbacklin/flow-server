d7645441552a8ba9edc2dce0b91d8e0b830c6937
Johan Bäcklin <112544750+johanbacklin@users.noreply.github.com>
2023-04-17 15:38:34 +0200

    Changed the structureNow only username is required

--- src/controllers/validations/validateFollow.js
+++ src/controllers/validations/validateFollow.js
@@ -2,8 +2,7 @@ const joi = require("joi");
 
 exports.validateFollow = function (requestBody) {
   const followSchema = joi.object({
-    username: joi.string().required(),
-    _id: joi.string().required(),
+    username: joi.string().min(3).max(50).required(),
   });
 
   return followSchema.validate(requestBody);
