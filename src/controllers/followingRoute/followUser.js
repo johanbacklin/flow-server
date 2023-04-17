cb5e7ca5e7fbf0cc2577e3632cc0451383958f17
Johan Bäcklin <112544750+johanbacklin@users.noreply.github.com>
2023-04-17 15:34:40 +0200

    Updated followUser with less request for database

--- src/controllers/followingRoute/followUser.js
+++ src/controllers/followingRoute/followUser.js
@@ -1,6 +1,5 @@
 const { db } = require("../../database/database");
 const { validateFollow } = require("../validations/validateFollow");
-const { ObjectId } = require("mongodb");
 
 exports.followUser = async function (req, res) {
   const validatedBody = validateFollow(req.body);
@@ -10,28 +9,46 @@ exports.followUser = async function (req, res) {
     return;
   }
 
-  if (req.body._id !== req.loggedInUser.id) {
-    try {
-      const userToFollow = await db.users.findOne({
-        _id: new ObjectId(req.body._id),
-      });
-      if (userToFollow.following.includes(req.loggedInUser.username)) {
-        return res.status(403).json("You are already following this user");
-      } else {
-        await db.users.updateOne(
-          { _id: new ObjectId(req.body._id) },
-          { $push: { following: req.loggedInUser.username } }
-        );
-        await db.users.updateOne(
-          { _id: new ObjectId(req.loggedInUser.id) },
-          { $push: { followers: userToFollow.username } }
-        );
-        res.status(200).json("User has been followed");
-      }
-    } catch (err) {
-      return res.status(500).json(err);
+  if (req.body.username === req.loggedInUser.username) {
+    return res.status(400).json("You cannot follow yourself");
+  }
+
+  try {
+    const userToFollow = await db.users.findOne({
+      username: req.body.username,
+    });
+
+    if (!userToFollow) {
+      return res.status(404).json("User not found");
     }
-  } else {
-    return res.status(400).json("You can't follow yourself");
+
+    if (userToFollow.followers.includes(req.loggedInUser.username)) {
+      return res.status(403).json("You are already following this user");
+    }
+
+    const update = [
+      {
+        updateOne: {
+          filter: { username: req.body.username },
+          update: { $addToSet: { followers: req.loggedInUser.username } },
+        },
+      },
+      {
+        updateOne: {
+          filter: { username: req.loggedInUser.username },
+          update: { $addToSet: { following: req.body.username } },
+        },
+      },
+    ];
+
+    const result = await db.users.bulkWrite(update);
+
+    if (result.modifiedCount === 0) {
+      return res.status(404).json("User not found");
+    }
+
+    return res.status(200).json("User has been followed");
+  } catch (err) {
+    return res.status(500).json(err.message);
   }
 };
