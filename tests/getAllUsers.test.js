3e9465e1e815056643e4d0f4146839b3d80a2bd0
Andreas.Warncke <andreas.warncke.1996@gmail.com>
2023-04-17 11:28:14 +0200

    updated test for GetAllUsers

--- tests/getAllUsers.test.js
+++ tests/getAllUsers.test.js
@@ -2,23 +2,20 @@ const request = require("supertest");
 const { db } = require("../src/database/database");
 const { server } = require("../src/server/server");
 
-async function connect() {
+beforeAll(async () => {
   await db.connect();
-}
-connect();
-
-describe("getAllUsers", () => {
-  test("returns a 200", async () => {
-    const response = await request(server).get("/authentication/users");
-
-    expect(response.status).toBe(500);
-  });
 });
 
-async function disconnect() {
+afterAll(async () => {
   await db.disconnect();
-}
-disconnect();
+});
 
-/* expect(response.body).toHaveProperty("usernames");
-expect(Array.isArray(response.body.usernames)).toBe(true); */
+describe("getAllUsers", () => {
+  test("returns a array with a property of usernames and 200 as status", async () => {
+    const response = await request(server).get("/authentication/users");
+    expect(response.body).toHaveProperty("usernames");
+    expect(Array.isArray(response.body.usernames)).toBe(true);
+
+    expect(response.status).toBe(200);
+  });
+});
