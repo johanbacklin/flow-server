15ecbab51fb4b9e892c6bee7fa52ae140793f4b4
Mika Munterud <mika.munterud@outlook.com>
2023-04-17 10:21:10 +0200

    Changing validation name for validating post id

--- src/controllers/validations/validatePostID.js
+++ src/controllers/validations/validatePostID.js
@@ -1,6 +1,6 @@
 const joi = require("joi");
 
-exports.validateDatabaseID = function (requestBody) {
+exports.validatePostID = function (requestBody) {
   const postSchema = joi.object({
     id: joi.string().length(24).hex().required(),
   });
