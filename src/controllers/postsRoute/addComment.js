0f1d332ca5932c17b32d08098c91708652a90994
Andreas.Warncke <andreas.warncke.1996@gmail.com>
2023-04-18 13:01:25 +0200

    created addComment endpoint

---
+++ src/controllers/postsRoute/addComment.js
@@ -0,0 +1,36 @@
+const { ObjectId } = require("mongodb");
+const { db } = require("../../database/database");
+const { validateComment } = require("../validations/validateComment");
+
+exports.addComment = async function (request, response) {
+  try {
+    const { username } = request.loggedInUser;
+
+    const validatedBody = validateComment(request.body);
+
+    if (validatedBody.error) {
+      response.status(400).send(validatedBody.error.details[0].message);
+      return;
+    }
+
+    const { commentText, postID } = validatedBody.value;
+
+    const result = await db.posts.updateOne(
+      { _id: new ObjectId(postID) },
+      {
+        $push: {
+          comments: {
+            username: username,
+            comment: commentText,
+          },
+        },
+      }
+    );
+
+    response.status(200).json(result);
+  } catch (error) {
+    response
+      .status(500)
+      .json({ error: "Internal server error: could not add comment" });
+  }
+};
