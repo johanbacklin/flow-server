b9cdde58aef9fbc747ec7a3066a41352843d5720
Andreas.Warncke <andreas.warncke.1996@gmail.com>
2023-04-19 17:21:46 +0200

    updated files

--- tests/deletePost.test.js
+++ tests/deletePost.test.js
@@ -1,4 +1,3 @@
-const { response } = require("express");
 const request = require("supertest");
 const { db } = require("../src/database/database");
 const { server } = require("../src/server/server");
@@ -13,9 +12,19 @@ afterAll(async () => {
 
 describe("deletePost", () => {
   test("should return 400 if invalid post id", async () => {
+    const loginResponse = await request(server)
+      .post("/authentication/login")
+      .send({
+        username: "andreas",
+        password: "andreas",
+      });
+
+    const cookie = loginResponse.header["set-cookie"].pop();
+
     const response = await request(server)
       .delete("/posts/delete")
-      .send({ id: "1" });
+      .send({ id: "1" })
+      .set("Cookie", [cookie]);
 
     expect(response.statusCode).toBe(400);
   });
