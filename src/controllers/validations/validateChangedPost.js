48b2d91598f5ec08bfbc2b8a273e5b40be87641b
TereseBo <terese.boderus@gmail.com>
2023-04-19 16:13:19 +0200

    Branch updated to match current main branch

---
+++ src/controllers/validations/validateChangedPost.js
@@ -0,0 +1,11 @@
+
+const joi = require("joi");
+
+exports.validateChangedPost = function (requestBody) {
+  const postSchema = joi.object({
+    postText: joi.string().min(3).max(500).required(),
+    id: joi.string().hex().length(24).required()
+  });
+
+  return postSchema.validate(requestBody);
+};
