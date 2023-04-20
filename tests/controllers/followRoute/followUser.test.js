e1978ad0a1a019379c29d67c3d3c37552823bc18
Johan Bäcklin <112544750+johanbacklin@users.noreply.github.com>
2023-04-20 10:03:19 +0200

    Created test for followUser endpoint

---
+++ tests/controllers/followRoute/followUser.test.js
@@ -0,0 +1,66 @@
+const request = require("supertest");
+const { db } = require("../../../src/database/database");
+const { server } = require("../../../src/server/server");
+
+beforeAll(async function () {
+  await db.connect();
+});
+
+afterAll(async function () {
+  await db.disconnect();
+});
+
+describe("Testing followUser endpoint", () => {
+  test("POST /following/follow should return 200 if user followed successfully", async () => {
+    const logInResponse = await request(server)
+      .post("/authentication/login")
+      .send({
+        username: "test",
+        password: "testingAccount",
+      });
+    const cookie = logInResponse.header["set-cookie"].pop();
+
+    const response = await request(server)
+      .post("/following/follow")
+      .send({ username: "Mika" })
+      .set("Cookie", cookie);
+
+    expect(response.status).toBe(200);
+
+    await request(server)
+      .delete("/following/unFollow")
+      .send({ username: "Mika" })
+      .set("Cookie", cookie);
+  });
+
+  test("POST /following/follow should return 400 if user tries to follow themselves", async () => {
+    const logInResponse = await request(server)
+      .post("/authentication/login")
+      .send({ username: "test", password: "testingAccount" });
+
+    const cookie = logInResponse.header["set-cookie"].pop();
+    const response = await request(server)
+      .post("/following/follow")
+      .send({ username: "test" })
+      .set("Cookie", cookie);
+
+    expect(response.status).toBe(400);
+  });
+
+  test("POST /following/follow should return 404 if user tries to follow a non-existent user", async () => {
+    const loginResponse = await request(server)
+      .post("/authentication/login")
+      .send({
+        username: "test",
+        password: "testingAccount",
+      });
+
+    const cookie = loginResponse.header["set-cookie"];
+    const response = await request(server)
+      .post("/following/follow")
+      .send({ username: "test3" })
+      .set("Cookie", cookie);
+
+    expect(response.status).toBe(404);
+  });
+});
