563ce9f7ec6981debed069eb2553f908d6d60598
Mika Munterud <mika.munterud@outlook.com>
2023-04-21 01:42:53 +0200

    Revert "Updating userRegister testing where example users are created and removed after testing"
    This reverts commit 196a0e52a46beb2a540491745d0cd42b5e7ac658.

--- tests/controllers/authenticationRoute/userRegister.test.js
+++ tests/controllers/authenticationRoute/userRegister.test.js
@@ -4,35 +4,9 @@ const { server } = require("../../../src/server/server");
 
 beforeAll(async function () {
   await db.connect();
-
-  await request(server)
-    .post("/authentication/register")
-    .send({ username: "ManyBrownies", password: "Chocolate" });
 });
 
 afterAll(async function () {
-  const response = await request(server)
-    .post("/authentication/login")
-    .send({ username: "Brownies", password: "Chocolate" });
-
-  const cookie = response.header["set-cookie"].pop();
-
-  await request(server)
-    .delete("/authentication/deleteUser")
-    .send({ username: "Brownies" })
-    .set("Cookie", cookie);
-
-  const response2 = await request(server)
-    .post("/authentication/login")
-    .send({ username: "ManyBrownies", password: "Chocolate" });
-
-  const cookie2 = response2.header["set-cookie"].pop();
-
-  await request(server)
-    .delete("/authentication/deleteUser")
-    .send({ username: "ManyBrownies" })
-    .set("Cookie", cookie2);
-
   await db.disconnect();
 });
 
@@ -44,17 +18,28 @@ describe("Testing userRegister endpoint", function () {
     expect(response.status).toBe(400);
   });
 
-  test("POST /authentication/register should return 409 if username already exists", async function () {
-    const response = await request(server)
-      .post("/authentication/register")
-      .send({ username: "ManyBrownies", password: "Chocolate" });
-    expect(response.status).toBe(409);
-  });
-
   test("POST /authentication/register should return 201 if successful registration", async function () {
     const response = await request(server)
       .post("/authentication/register")
-      .send({ username: "Brownies", password: "Chocolate" });
+      .send({ username: "Brownie", password: "Chocolate" });
     expect(response.status).toBe(201);
+  });
+
+  test("DELETE /authentication/deleteUser should return 200 if user is deleted", async function () {
+    const loginResponse = await request(server)
+      .post("/authentication/login")
+      .send({
+        username: "Brownie",
+        password: "Chocolate",
+      });
+
+    const cookie = await loginResponse.header["set-cookie"].pop();
+
+    const response = await request(server)
+      .delete("/authentication/deleteUser")
+      .send({ username: "Brownie" })
+      .set("Cookie", cookie);
+
+    expect(response.status).toBe(200);
   });
 });
