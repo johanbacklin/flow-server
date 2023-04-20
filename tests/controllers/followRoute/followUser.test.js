ed70fd697be00934bea32071b314fa864bebf45d
Johan Bäcklin <112544750+johanbacklin@users.noreply.github.com>
2023-04-20 13:02:10 +0200

    Moving unFollow testing to followTesting

--- tests/controllers/followRoute/followUser.test.js
+++ tests/controllers/followRoute/followUser.test.js
@@ -58,4 +58,52 @@ describe("Testing followUser endpoint", () => {
 
     expect(response.status).toBe(404);
   });
+
+  test("DELETE /following/unFollow should return 200 if user unfollowed successfully", async () => {
+    const logInResponse = await request(server)
+      .post("/authentication/login")
+      .send({
+        username: "test",
+        password: "testingAccount",
+      });
+    const cookie = logInResponse.header["set-cookie"].pop();
+
+    const responseUnFollow = await request(server)
+      .delete("/following/unFollow")
+      .send({ username: "Mika" })
+      .set("Cookie", cookie);
+
+    expect(responseUnFollow.status).toBe(200);
+  });
+
+  test("DELETE /following/unFollow should return 400 if user tries to unfollow themselves", async () => {
+    const logInResponse = await request(server)
+      .post("/authentication/login")
+      .send({ username: "test", password: "testingAccount" });
+
+    const cookie = logInResponse.header["set-cookie"].pop();
+    const response = await request(server)
+      .delete("/following/unFollow")
+      .send({ username: "test" })
+      .set("Cookie", cookie);
+
+    expect(response.status).toBe(400);
+  });
+
+  test("DELETE /following/unfollow should return 404 if user tries to unfollow a non-existent user", async () => {
+    const loginResponse = await request(server)
+      .post("/authentication/login")
+      .send({
+        username: "test",
+        password: "testingAccount",
+      });
+
+    const cookie = loginResponse.header["set-cookie"];
+    const response = await request(server)
+      .delete("/following/unFollow")
+      .send({ username: "test3" })
+      .set("Cookie", cookie);
+
+    expect(response.status).toBe(404);
+  });
 });
