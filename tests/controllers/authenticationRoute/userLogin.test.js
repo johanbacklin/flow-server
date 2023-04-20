081d732a64f7387a673cb61defed061d55a3f34d
Mika Munterud <mika.munterud@outlook.com>
2023-04-21 01:45:02 +0200

    Revert "Updating userLogin testing where example user is created and removed after testing"
    This reverts commit ed66e823c08b31bc6416509f740527a6d9297646.

--- tests/controllers/authenticationRoute/userLogin.test.js
+++ tests/controllers/authenticationRoute/userLogin.test.js
@@ -2,21 +2,11 @@ const request = require("supertest");
 const { server } = require("../../../src/server/server");
 const { db } = require("../../../src/database/database");
 
-let cookie = null;
-
 beforeAll(async function () {
   await db.connect();
-  await request(server)
-    .post("/authentication/register")
-    .send({ username: "Brownie", password: "Chocolate" });
 });
 
 afterAll(async function () {
-  await request(server)
-    .delete("/authentication/deleteUser")
-    .send({ username: "Brownie" })
-    .set("Cookie", cookie);
-
   await db.disconnect();
 });
 
@@ -31,17 +21,14 @@ describe("Testing userLogin endpoint", function () {
   test("POST /authentication/login should return 404 if username does not exist", async function () {
     const response = await request(server)
       .post("/authentication/login")
-      .send({ username: "Brownies!", password: "Chocolate" });
+      .send({ username: "tester", password: "testingAccount" });
     expect(response.status).toBe(404);
   });
 
   test("POST /authentication/login should return 200 if good login", async function () {
     const response = await request(server)
       .post("/authentication/login")
-      .send({ username: "Brownie", password: "Chocolate" });
-
-    cookie = response.header["set-cookie"].pop();
-
+      .send({ username: "test", password: "testingAccount" });
     expect(response.status).toBe(200);
   });
 });
