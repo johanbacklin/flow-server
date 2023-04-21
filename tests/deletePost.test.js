35e084dd8895acbb4f3026478e15c49a5eed19e7
Andreas.Warncke <andreas.warncke.1996@gmail.com>
2023-04-21 11:26:55 +0200

    updated test
    added a new account when test starts and remove when done

--- tests/deletePost.test.js
+++ tests/deletePost.test.js
@@ -1,26 +1,33 @@
 const request = require("supertest");
 const { db } = require("../src/database/database");
 const { server } = require("../src/server/server");
+let cookie = undefined;
 
 beforeAll(async () => {
   await db.connect();
+  const resReg = await request(server)
+    .post("/authentication/register")
+    .send({ username: "TestDelete", password: "TestDelete" });
+  if (resReg.status == 201) {
+    const resLog = await request(server)
+      .post("/authentication/login")
+      .send({ username: "TestDelete", password: "TestDelete" });
+    cookie = resLog.header["set-cookie"];
+  }
 });
 
 afterAll(async () => {
-  await db.disconnect();
+  try {
+    await db.posts.deleteMany({ username: "TestDelete" });
+    await db.users.deleteOne({ username: "TestDelete" });
+  } catch (error) {
+  } finally {
+    await db.disconnect();
+  }
 });
 
 describe("deletePost", () => {
   test("should return 400 if invalid post id", async () => {
-    const loginResponse = await request(server)
-      .post("/authentication/login")
-      .send({
-        username: "andreas",
-        password: "andreas",
-      });
-
-    const cookie = loginResponse.header["set-cookie"].pop();
-
     const response = await request(server)
       .delete("/posts/delete")
       .send({ id: "1" })
