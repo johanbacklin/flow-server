12f1a65c1a486edb06559e56f4b4a4f7440f25a6
Mika Munterud <mika.munterud@outlook.com>
2023-04-21 02:24:52 +0200

    Adding testing for bad possible outcome in postGet endpoint

--- tests/controllers/postRoute/postGet.test.js
+++ tests/controllers/postRoute/postGet.test.js
@@ -5,13 +5,22 @@ const { db } = require("../../../src/database/database");
 beforeAll(async function () {
   await db.connect();
 });
-
 afterAll(async function () {
   await db.disconnect();
 });
 
 describe("Testing postGet endpoint", function () {
-  test("GET /posts/:username should return 401 if no cookie exists", async function () {
+  test("GET /posts/:username should return 400 if body is sent", async function () {
+    const response = await request(server).get("/posts/test").send({ id: 1 });
+    expect(response.status).toBe(400);
+  });
+
+  test("GET /posts/:username should return 404 if no posts exist for the inputted username", async function () {
+    const response = await request(server).get("/posts/test/Chocolate");
+    expect(response.status).toBe(404);
+  });
+
+  test("GET /posts/:username should return 200 if successful get", async function () {
     const response = await request(server).get("/posts/test");
     expect(response.status).toBe(200);
   });
