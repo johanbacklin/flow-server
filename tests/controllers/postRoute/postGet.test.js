c3acc9c6ce98a95e2ffaae7afa9babc94a3e033c
Mika Munterud <mika.munterud@outlook.com>
2023-04-24 12:01:09 +0200

    Adding testAccount to testing

--- tests/controllers/postRoute/postGet.test.js
+++ tests/controllers/postRoute/postGet.test.js
@@ -2,10 +2,25 @@ const request = require("supertest");
 const { server } = require("../../../src/server/server");
 const { db } = require("../../../src/database/database");
 
+let cookie = null;
+
 beforeAll(async function () {
   await db.connect();
+  await request(server)
+    .post("/authentication/register")
+    .send({ username: "Chocolate2", password: "Chocolate" });
+
+  const response = await request(server)
+    .post("/authentication/login")
+    .send({ username: "Chocolate2", password: "Chocolate" });
+
+  cookie = response.header["set-cookie"].pop();
 });
 afterAll(async function () {
+  await request(server)
+    .delete("/authentication/deleteUser")
+    .send({ username: "Chocolate2" })
+    .set("Cookie", cookie);
   await db.disconnect();
 });
 
@@ -16,7 +31,7 @@ describe("Testing postGet endpoint", function () {
   });
 
   test("GET /posts/:username should return 404 if no posts exist for the inputted username", async function () {
-    const response = await request(server).get("/posts/test/Chocolate");
+    const response = await request(server).get("/posts/Chocolate2");
     expect(response.status).toBe(404);
   });
 
