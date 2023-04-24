8aef21d59be47219c624e9e2accda0b16eb56eb6
Mika Munterud <mika.munterud@outlook.com>
2023-04-24 12:00:50 +0200

    Removing duplicate endpoint

--- src/routes/postsRoute.js
+++ src/routes/postsRoute.js
@@ -25,11 +25,11 @@ postsRoute.get("/:username", postGet);
 
 postsRoute.post("/comment", addComment);
 
-postsRoute.post("/like", postLike);
 postsRoute.delete("/delete", checkAuthentication, deletePost);
 
 postsRoute.post("/like", checkAuthentication, postLike);
 
 postsRoute.delete("/like", checkAuthentication, postLikeDelete);
-postsRoute.patch("/",checkAuthentication, updatePost);
+
+postsRoute.patch("/", checkAuthentication, updatePost);
 exports.postsRoute = postsRoute;
